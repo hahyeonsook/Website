4824abff3dfede76b78cce6adfb869c90ea559e0
hyunsuck <qu3230@gmail.com>
2018-11-06 01:55:01 +0900

    사용자별 북마크로 수정

--- website/bookmarks/views.py
+++ website/bookmarks/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
-from bookmarks.models import Bookmarks, Link
+from bookmarks.models import Bookmark, Link
 from django.views import generic
 
 # Create your views here.
 
+
 class IndexView(generic.ListView):
+    model = Bookmark
     template_name = 'bookmarks/index.html'
     context_object_name = 'latest_links_list'
 
     def get_queryset(self):
-        """최신순으로 링크 나열"""
+        # 최신순으로 링크 나열
         return Link.objects.order_by('-id')
--- website/posts/models.py
+++ website/posts/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    user = models.ForeignKey(setting.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ForeignKey('Image', on_delete=models.CASCADE)
-    tags = models.ManyToManyField('Tag')
+#    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
 
 class Image(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to='post')
+
+    def __str__(self):
+        return self.pk
+"""
+class Tag(models.Model):
+    post = models.ManyToManyField('Tag')
+"""

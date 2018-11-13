9bfd4b564517ddd45ca5138dc23dbe65979e18e1
hyunsuck <qu3230@gmail.com>
2018-11-13 18:23:21 +0900

    게시물 폼 수정

--- website/posts/form.py
+++ website/posts/form.py
@@ -2,5 +2,5 @@ from django import forms
 
 class UploadPostForm(forms.Form):
     title = forms.CharField(max_length=50)
-    image = forms.FileField()
+    image = forms.ImageField()
     content = forms.CharField(max_length=80)
